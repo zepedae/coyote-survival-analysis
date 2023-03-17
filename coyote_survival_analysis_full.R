1a25ffbc1f0f980223d7b14d486f2c0fba87557b
Emily Zepeda <emilyazepeda@gmail.com>
2023-03-17 13:31:59 -0500

    Clean code

--- coyote_survival_analysis_full.R
+++ coyote_survival_analysis_full.R
@@ -1,12 +1,12 @@
 ######### LOAD PACKAGES ###########
+
 library(lubridate)
 library(dplyr)
 library(tidyr)
-
 library(raster)
 library(sp)
-
 library(survival)
+
 
 
 ########## PREPARE MORTALITY DATA ############
@@ -20,12 +20,13 @@ colnames(mortality)[colnames(mortality) == "Date"] <- "mort_date"
 colnames(mortality)[colnames(mortality) == "Mortality.cause"] <- "cause"
 
 mortality$mort_date <- mdy(mortality$mort_date)
-mortData <- subset(mortality, select=c("id", "mort_date", "easting", "northing", "cause"))
+mort_data <- subset(mortality, select=c("id", "mort_date", "easting", "northing", 
+                                        "cause"))
 
 # remove any animals with more than one mort date
-mortData[duplicated(mortData$id),] # so remove the "not dead" entry for 173 and 
+mort_data[duplicated(mort_data$id),] # so remove the "not dead" entry for 173 and 
  # both 453s b/c can't determine which is correct
-mortData <- mortData[mortData$cause != "Not dead" & mortData$id != 453,]
+mort_data <- mort_data[mort_data$cause != "Not dead" & mort_data$id != 453,]
 
 
 ########## PREPARE UTM DATA ############
@@ -44,11 +45,11 @@ colnames(utm)[colnames(utm) == "Northing"] <- "northing"
 # create a status column that indicates whether the animal was censored 
  # i.e. not recovered post-mortem (0) or confirmed dead (1)
 utm <- left_join(utm[, c("id", "northing", "easting", "date")], 
-             mortData[, c("id", "mort_date")], by = "id", all.x = TRUE) 
+             mort_data[, c("id", "mort_date")], by = "id", all.x = TRUE) 
 utm$status <- ifelse(is.na(utm$mort_date), 0, 1)
 
 # remove any points that were taken after mort_date
-utm$false_points <- ifelse(is.na(utm$mort_date) | utm$date < utm$mort_date , 0, 1)
+utm$false_points <- ifelse(is.na(utm$mort_date) | utm$date <= utm$mort_date, 0, 1)
 utm_false <- utm[utm$false_points == 1,]
 utm <- utm[utm$false_points == 0,]
 
@@ -63,22 +64,26 @@ birth_ids <- pup_parent_ids$coyote_id
 utm <- utm[utm$id %in% birth_ids,]
 
 # create an "end date" that's either the mort_date or last date of tracking, whichever comes last
-max_date <- utm %>% group_by(id) %>% summarise(max(date))
-utm <- left_join(utm, max_date, by = "id")
-utm$end_date <- data.table::fifelse(is.na(utm$mort_date), utm$`max(date)`, utm$mort_date)
+utm <- utm %>% 
+  group_by(id) %>% 
+  mutate(max_date = max(date), 
+         end_date = data.table::fifelse(is.na(mort_date), 
+                               max(date), mort_date)) %>% ungroup                                                      
+                                                                          
+# calculate survival time 
+utm <- pup_parent_ids %>% 
+  dplyr::select(coyote_id, birth_year) %>%
+  mutate(birth_year = mdy(paste("05-01-", birth_year))) %>% 
+  left_join(., utm, by = c("coyote_id" = "id")) %>% 
+  mutate(surv_time = as.numeric(difftime(end_date, birth_year))) %>% ungroup()
 
-# survival time in days
-birthdays <- pup_parent_ids[,c("coyote_id", "birth_year")]
-birthdays$birth_year <- paste("05-01-", birthdays$birth_year) # use average coyote birthday
-birthdays$birth_year <- mdy(birthdays$birth_year)
-utm <- left_join(utm, birthdays, by = c("id" = "coyote_id"))
-utm$survTime <- difftime(utm$end_date, utm$birth_year)
+
 
 
 ######### FILTER OUT INDIVIDUALS WITH TOO FEW LOCATIONS ############
 
 # remove individuals whose last tracking day was more than 1 month from mortality
-utm$gap <- as.numeric(difftime(utm$end_date-1, utm$`max(date)`, units = "days"))
+utm$gap <- as.numeric(difftime(utm$end_date-1, utm$max_date, units = "days"))
 utm <- utm[utm$gap < 30,]
 
 # global analysis includes data from an animal's entire life
@@ -92,10 +97,12 @@ utmGlobal <- utm[utm$id %in% over60,]
   # months of death or tracking if they weren't recovered post mortem
 utmLocal <- utm[utm$date > utm$end_date - 180,]
 length(unique(utmLocal$id))
-subLocations30 <- utmLocal %>% group_by(id) %>% tally()
+subLocations30 <- utmLocal %>% group_by(id) %>% tally() %>% ungroup()
 over30 <- ifelse(subLocations30$n > 30, subLocations30$id, NA)
 over30 <- na.omit(over30)
-utmLocal <- utm[utm$id %in% over30,]
+utmLocal <- utmLocal[utmLocal$id %in% over30,]
+
+
 
 
 ########## EXTRACT SOCIAL AND ENVIRONMENTAL CHARACTERISTICS ###########
@@ -134,12 +141,15 @@ localValsST <- localValues %>% group_by(id) %>%
 
 ###########  COMBINE SURVIVAL AND SOCIAL/ENVIRONMENTAL DATA  ##########
 
-surv_data <- unique(utm[, c("id", "status", "survTime")])
+surv_data <- unique(utm[, c("id", "status", "surv_time")])
 global_df <- merge(globalValsST, surv_data, by = "id")
 local_df <- merge(localValsST, surv_data, by = "id")
 
 
 ############  COX PROPORTIONAL HAZARDS MODEL  #############
 
-global_fit <- coxph(Surv(survTime, status) ~ medInc + popDens + nat + dist + propW, data = global_df)
-local_fit <- coxph(Surv(survTime, status) ~ medInc + popDens + nat + dist, data = local_df)
+global_fit <- coxph(Surv(surv_time, status) ~ medInc + popDens + nat + dist + propW, data = global_df)
+local_fit <- coxph(Surv(surv_time, status) ~ medInc + popDens + nat + dist, data = local_df)
+
+c("id", "date", "time", "easting", "northing", 
+  "mort_date", "status")
